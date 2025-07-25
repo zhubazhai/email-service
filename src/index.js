b9621cc638c052beeba371836998cab9f51fee1c
ex_lingfeng.meng <ex_lingfeng.meng@tcl.com>
2025-07-25 21:31:33 +0800

    fix: 修复一下问题 585

--- src/index.js
+++ src/index.js
@@ -1,41 +1,71 @@
 require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
-app.use(express.json());
 
-// 创建邮件发送函数
-const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "SendGrid",
-    auth: {
-      user: "apikey",
-      //   pass: process.env.SENDGRID_API_KEY,
-      pass: "ruravkqypocjjcaj",
-    },
-  });
+// 配置文件存储
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "static/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// 配置 QQ 邮箱的 SMTP 服务
+const transporter = nodemailer.createTransport({
+  host: "smtp.qq.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.QQ_EMAIL,
+    pass: process.env.QQ_EMAIL_PASSWORD,
+  },
+});
+
+// 定义发送邮件的 API
+app.post("/api/send-email", upload.single("file"), (req, res) => {
+  const { to, subject, text, name } = req.body;
+
+  // 获取上传的文件路径
+  const attachmentPath = path.join(__dirname, "static", req.file.filename);
 
   const mailOptions = {
-    from: "1149541057@qq.com",
+    from: process.env.QQ_EMAIL,
     to,
     subject,
     text,
+    attachments: [
+      {
+        filename: name,
+        path: attachmentPath,
+      },
+    ],
   };
 
-  return transporter.sendMail(mailOptions);
-};
-
-// 定义发送邮件的 API
-app.post("/api/send-email", async (req, res) => {
-  const { to, subject, text } = req.body;
-
-  try {
-    await sendEmail(to, subject, text);
-    res.status(200).send("Email sent successfully");
-  } catch (error) {
-    res.status(500).send("Error sending email");
-  }
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res.status(500).send(`Error sending email: ${error.message}`);
+    } else {
+      // 发送成功后删除文件
+      fs.unlink(attachmentPath, (deleteError) => {
+        if (deleteError) {
+          console.error("Error deleting file:", deleteError);
+          res.status(500).send(`Error deleting file: ${deleteError.message}`);
+        } else {
+          res.status(200).send("Email sent successfully and file deleted");
+        }
+      });
+    }
+  });
 });
 
-module.exports = app;
+module.exports = { app };
